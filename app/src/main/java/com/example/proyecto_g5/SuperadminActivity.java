80f3045afe425d3c3eb4c6917995bd21f5d0e39c
william <william24jes@hotmail.com>
2024-04-30 02:04:31 -0500

    IDS superadmin

--- app/src/main/java/com/example/proyecto_g5/SuperadminActivity.java
+++ app/src/main/java/com/example/proyecto_g5/SuperadminActivity.java
@@ -1,6 +1,27 @@
 package com.example.proyecto_g5;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SuperadminActivity extends AppCompatActivity {
+
+    private TextView textViewBienvenidoSA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.superadmin_inicio);
+        super.onCreate(savedInstanceState);
+
+        textViewBienvenidoSA = findViewById(R.id.textViewBienvenidoSA);
+
+        // Obtener información del intent
+        String nombre = getIntent().getStringExtra("nombre");
+        String apellido = getIntent().getStringExtra("apellido");
+
+        // Actualizar el texto del TextView
+        textViewBienvenidoSA.setText("¡Bienvenido Administrador " + nombre + " " + apellido + "!");
+    }
 }
