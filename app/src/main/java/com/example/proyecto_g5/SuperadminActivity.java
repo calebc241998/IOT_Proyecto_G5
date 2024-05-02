1f451ccc7ad260d96368b979dba62c2db99b090c
Daniel <a20180805@pucp.edu.pe>
2024-05-02 17:06:54 -0500

    idk

--- app/src/main/java/com/example/proyecto_g5/SuperadminActivity.java
+++ app/src/main/java/com/example/proyecto_g5/SuperadminActivity.java
@@ -1,19 +1,28 @@
 package com.example.proyecto_g5;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.proyecto_g5.databinding.ActivityNavigationDrawerBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class SuperadminActivity extends AppCompatActivity {
 
     private TextView SAItextViewBienvenido;
 
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityNavigationDrawerBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        setContentView(R.layout.superadmin_inicio);
-        super.onCreate(savedInstanceState);
 
         SAItextViewBienvenido = findViewById(R.id.SAItextViewBienvenido);
 
@@ -23,5 +32,37 @@ public class SuperadminActivity extends AppCompatActivity {
 
         // Actualizar el texto del TextView
         SAItextViewBienvenido.setText("¡Bienvenido Superadmin " + nombre + " " + apellido + "!");
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityNavigationDrawerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarNavigationDrawer.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.superadmin_inicio, R.id.superadmin_lista_usuarios, R.id.superadmin_nuevo_admin, R.id.superadmin_logs)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.superadmin_nav_host_fragment_content_navigation_drawer);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.superadmin_nav_host_fragment_content_navigation_drawer);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
