2187d93abcc56c63ad797e92de4d11afea70f18f
william <william24jes@hotmail.com>
2024-06-04 15:38:16 -0500

    ya hay logs de sitios

--- app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_sitios.java
+++ app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_sitios.java
@@ -17,39 +17,22 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.proyecto_g5.R;
-import com.example.proyecto_g5.Recycler.Supervisor.ListarSitiosXML.DataListaSitiosClass;
-import com.example.proyecto_g5.Recycler.Supervisor.ListarSitiosXML.MyAdapterListaSitios;
 import com.example.proyecto_g5.databinding.SupervisorListaSitiosBinding;
 import com.example.proyecto_g5.dto.Sitio;
-import com.example.proyecto_g5.dto.Usuario;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link supervisor_lista_sitios#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class supervisor_lista_sitios extends Fragment implements MyAdapterListaSitios.OnItemClickListener {
+public class supervisor_lista_sitios extends Fragment {
 
     RecyclerView recyclerView;
-    List<Sitio> datalist;
-    MyAdapterListaSitios adapter;
-    Sitio androidData;
-    SupervisorListaSitiosBinding supervisorListaSitiosBinding;
     FirebaseFirestore db;
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -57,15 +40,6 @@ public class supervisor_lista_sitios extends Fragment implements MyAdapterListaS
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment supervisor_lista_sitios.
-     */
-    // TODO: Rename and change types and number of parameters
     public static supervisor_lista_sitios newInstance(String param1, String param2) {
         supervisor_lista_sitios fragment = new supervisor_lista_sitios();
         Bundle args = new Bundle();
@@ -86,47 +60,23 @@ public class supervisor_lista_sitios extends Fragment implements MyAdapterListaS
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        supervisorListaSitiosBinding = SupervisorListaSitiosBinding.inflate(inflater, container, false);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        SupervisorListaSitiosBinding supervisorListaSitiosBinding = SupervisorListaSitiosBinding.inflate(inflater, container, false);
         db = FirebaseFirestore.getInstance();
-        supervisorListaSitiosBinding.BuscarSitios.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                searchList(newText);
-                return false;
-            }
-        });
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
         recyclerView = supervisorListaSitiosBinding.recyclerViewOficial;
         recyclerView.setLayoutManager(gridLayoutManager);
-        datalist = new ArrayList<>();
 
-        androidData = new Sitio("Real Plaza",null, "Lima","Lima","San Miguel",null,null,null,null,null);
-        datalist.add(androidData);
-
-        androidData = new Sitio("Mall Plaza",null, "Lima","Lima","SJL",null,null,null,null,null);
-        datalist.add(androidData);
-
-
-        adapter = new MyAdapterListaSitios(getActivity(), datalist, this);
-        recyclerView.setAdapter(adapter);
-
-
+        // Obtención de datos de Firestore
         db.collection("usuarios")
-                .whereEqualTo("nombre", "William")  // Filtra por el nombre del usuario
+                .whereEqualTo("nombre", "william")  // Filtra por el nombre del usuario
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         for (QueryDocumentSnapshot document : task.getResult()) {
+                            Log.d("msg-test", "Usuario encontrado: " + document.getId());
+
                             // Accede a la subcolección "sitios" del usuario "William"
                             db.collection("usuarios")
                                     .document(document.getId())
@@ -145,31 +95,12 @@ public class supervisor_lista_sitios extends Fragment implements MyAdapterListaS
                                     });
                         }
                     } else {
-                        Toast.makeText(getContext(), "El usuario no existe", Toast.LENGTH_SHORT).show();
+                        Log.d("msg-test", "Error al obtener usuarios: ", task.getException());
+                        Toast.makeText(getContext(), "Error al obtener usuarios", Toast.LENGTH_SHORT).show();
                     }
                 });
 
 
-
-
-
-
         return supervisorListaSitiosBinding.getRoot();
-    }
-
-    @Override
-    public void onItemClick(Sitio item) {
-        NavController navController = NavHostFragment.findNavController(supervisor_lista_sitios.this);
-        navController.navigate(R.id.action_supervisor_lista_sitios_to_supervisor_descripcion_sitio);
-    }
-
-    private void searchList(String text) {
-        List<Sitio> dataSearchList = new ArrayList<>();
-        for (Sitio data : datalist) {
-            if (data.getNombre().toLowerCase().contains(text.toLowerCase())) {
-                dataSearchList.add(data);
-            }
-        }
-        adapter.setSearchList(dataSearchList);
     }
 }
