32175180665d18c1bbfcd5d6f0039139d908e0f3
william <william24jes@hotmail.com>
2024-06-04 21:53:22 -0500

    ya lista desde firebase con las nuevas tablas de ana

--- app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_sitios.java
+++ app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_sitios.java
@@ -1,31 +1,27 @@
 package com.example.proyecto_g5.Controladores.Supervisor;
 
 import android.util.Log;
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import com.example.proyecto_g5.R;
 import com.example.proyecto_g5.Recycler.Supervisor.ListarSitiosXML.MyAdapterListaSitios;
 import com.example.proyecto_g5.databinding.SupervisorListaSitiosBinding;
 import com.example.proyecto_g5.dto.Sitio;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class supervisor_lista_sitios extends Fragment implements MyAdapterListaSitios.OnItemClickListener{
+public class supervisor_lista_sitios extends Fragment implements MyAdapterListaSitios.OnItemClickListener {
 
     List<Sitio> datalist;
     MyAdapterListaSitios adapter;
@@ -57,7 +53,6 @@ public class supervisor_lista_sitios extends Fragment implements MyAdapterListaS
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -74,54 +69,28 @@ public class supervisor_lista_sitios extends Fragment implements MyAdapterListaS
         recyclerView.setLayoutManager(gridLayoutManager);
         datalist = new ArrayList<>();
 
-        androidData = new Sitio("Real Plaza",null, "Lima","Lima","San Miguel",null,null,null,null,null);
-        datalist.add(androidData);
-
-        androidData = new Sitio("Mall Plaza",null, "Lima","Lima","SJL",null,null,null,null,null);
-        datalist.add(androidData);
-
-
         adapter = new MyAdapterListaSitios(getActivity(), datalist, this);
         recyclerView.setAdapter(adapter);
 
-
-
         // Obtención de datos de Firestore
-        db.collection("usuarios")
-                .whereEqualTo("nombre", "william")  // Filtra por el nombre del usuario
+        db.collection("usuarios_por_auth")
+                .document("VDwqr0wPUsfHO8RhjvLPxRgWt3W2")
+                .collection("usuarios")
+                .document("william")
+                .collection("sitios")
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Log.d("msg-test", "Usuario encontrado: " + document.getId());
-
-                            // Accede a la subcolección "sitios" del usuario "William"
-                            db.collection("usuarios")
-                                    .document(document.getId())
-                                    .collection("sitios")
-                                    .get()
-                                    .addOnCompleteListener(task1 -> {
-                                        if (task1.isSuccessful()) {
-                                            for (QueryDocumentSnapshot sitioDoc : task1.getResult()) {
-                                                Sitio sitio = sitioDoc.toObject(Sitio.class);
-                                                Log.d("msg-test", "Sitio Nombre: " + sitio.getNombre());
-                                                Log.d("msg-test", "Sitio Lugar: " + sitio.getDistrito());
-
-                                                // Agregar el sitio a la lista datalist
-                                                datalist.add(sitio);
-                                            }
-                                            adapter.notifyDataSetChanged();
-                                        } else {
-                                            Log.d("msg-test", "Error al obtener sitios: ", task1.getException());
-                                        }
-                                    });
+                        for (DocumentSnapshot document : task.getResult()) {
+                            Sitio sitio = document.toObject(Sitio.class);
+                            datalist.add(sitio);
                         }
+                        adapter.notifyDataSetChanged();
                     } else {
-                        Log.d("msg-test", "Error al obtener usuarios: ", task.getException());
-                        Toast.makeText(getContext(), "Error al obtener usuarios", Toast.LENGTH_SHORT).show();
+                        Log.d("msg-test", "Error al obtener sitios: ", task.getException());
+                        Toast.makeText(getContext(), "Error al obtener sitios", Toast.LENGTH_SHORT).show();
                     }
                 });
-
 
         return supervisorListaSitiosBinding.getRoot();
     }
