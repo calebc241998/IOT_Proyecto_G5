b732152a2d0b1b390c07e806a75fddc495a2d028
Daniel <a20180805@pucp.edu.pe>
2024-06-20 05:23:11 -0500

    validaciones agregadas y vista crear admin

--- app/src/main/java/com/example/proyecto_g5/Controladores/Superadmin/superadmin_nuevo_admin.java
+++ app/src/main/java/com/example/proyecto_g5/Controladores/Superadmin/superadmin_nuevo_admin.java
@@ -50,17 +50,15 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class superadmin_nuevo_admin extends AppCompatActivity {
-
-
-    //----UPLOAD -----
 
     FirebaseFirestore db;
     ListenerRegistration snapshotListener;
     FirebaseUser currentUser;
-
-
     ImageView foto_perfil;
 
     Button boton_guardar_nuevoAdmin;
@@ -68,18 +66,12 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
     String canal1 = "importanteDefault";
 
     EditText nuevo_nombre, nuevo_apellido, nuevo_telefono, nuevo_direccion,nuevo_dni,nuevo_correo, nuevo_pass_superad;
-
     String imageUrl;
-
     Uri uri;
-
     DrawerLayout drawerLayout;
     ImageView menu, perfil;
-
     LinearLayout lista_usuarios, nuevo_admin, lista_logs, inicio_nav_superadmin, log_out;
-
     private TextView textViewBienvenido;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -107,28 +99,24 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openDrawer(drawerLayout);
             }
         });
-
         inicio_nav_superadmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 redirectActivity(superadmin_nuevo_admin.this, SuperadminActivity.class);
             }
         });
-
         lista_usuarios.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 redirectActivity(superadmin_nuevo_admin.this, superadmin_lista_usuarios.class);
             }
         });
-
         nuevo_admin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -141,7 +129,6 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
                 redirectActivity(superadmin_nuevo_admin.this, superadmin_logs.class);
             }
         });
-
         log_out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -152,10 +139,8 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
             }
         });
 
-
         db = FirebaseFirestore.getInstance();
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
 
         nuevo_nombre = findViewById(R.id.nombre_nuevoAdmin);
         nuevo_apellido = findViewById(R.id.apellido_nuevoAdmin);
@@ -194,23 +179,85 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
         });
 
         boton_guardar_nuevoAdmin.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
+                String nombre = nuevo_nombre.getText().toString().trim();
+                String apellido = nuevo_apellido.getText().toString().trim();
+                String telefono = nuevo_telefono.getText().toString().trim();
+                String direccion = nuevo_direccion.getText().toString().trim();
+                String dni = nuevo_dni.getText().toString().trim();
+                String correo = nuevo_correo.getText().toString().trim();
+                String pass_superad = nuevo_pass_superad.getText().toString().trim();
+
+                if (!validateInput(nombre, apellido, telefono, direccion, dni, correo, pass_superad)) {
+                    return; // stop further execution if validation fails
+                }
+
                 saveData();
-
             }
-
-
         });
+
+
 
     }
 
-    public void saveData(){
+    private boolean validateInput(String nombre, String apellido, String telefono, String direccion, String dni, String correo, String pass_superad) {
+        Pattern letterPattern = Pattern.compile("^[a-zA-Z]+$");
+        Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+        Pattern passwordPattern = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$");
+        Pattern addressPattern = Pattern.compile(".*\\d.*");
 
+        if (nombre.isEmpty() || apellido.isEmpty() || telefono.isEmpty() || direccion.isEmpty() || dni.isEmpty() || correo.isEmpty() || pass_superad.isEmpty()) {
+            Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!letterPattern.matcher(nombre).matches()) {
+            Toast.makeText(this, "El nombre solo debe contener letras", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!letterPattern.matcher(apellido).matches()) {
+            Toast.makeText(this, "El apellido solo debe contener letras", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (dni.length() != 8) {
+            Toast.makeText(this, "El teléfono debe contener exactamente 8 dígitos", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!emailPattern.matcher(correo).matches()) {
+            Toast.makeText(this, "El correo electrónico no tiene un formato válido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (telefono.length() != 9) {
+            Toast.makeText(this, "El teléfono debe contener exactamente 9 dígitos", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!addressPattern.matcher(direccion).matches()) {
+            Toast.makeText(this, "La dirección debe contener al menos un número", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        Matcher matcher = passwordPattern.matcher(pass_superad);
+        if (!matcher.matches()) {
+            Toast.makeText(this, "La contraseña debe contener al menos una letra y un número", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void saveData() {
         notificarImportanceDefault();
 
+        if (uri == null) {
+            Toast.makeText(superadmin_nuevo_admin.this, "Por favor, seleccione una imagen", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Usuario_imagen").child(uri.getLastPathSegment());
 
@@ -238,6 +285,7 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
         });
     }
 
+
     public  void uploadData( ){
         String nombre = nuevo_nombre.getText().toString();
         String apellido = nuevo_apellido.getText().toString();
@@ -252,7 +300,7 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
         String pass_superad = nuevo_pass_superad.getText().toString();
 
 
-        Usuario usuario = new Usuario(nombre, apellido, dni,correo, pass_superad, direccion, "administrador", "activo", imageUrl, telefono ,uid, correo_superad, pass_superad, "1");
+        Usuario usuario = new Usuario(nombre, apellido, dni,correo, pass_superad, direccion, "admin", "activo", imageUrl, telefono ,uid, correo_superad, pass_superad, "1");
 
         if(currentUser != null){
 
@@ -273,9 +321,6 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
         }
 
     }
-
-
-
 
     public static void openDrawer(DrawerLayout drawerLayout) {
         drawerLayout.openDrawer(GravityCompat.START);
@@ -323,8 +368,8 @@ public class superadmin_nuevo_admin extends AppCompatActivity {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, canal1)
                 .setSmallIcon(R.drawable.logo)
-                .setContentTitle("Nuevo Supervisor Guardado")
-                .setContentText("Ha sido creado un nuevo supervisor")
+                .setContentTitle("Nuevo Administrador Guardado")
+                .setContentText("Ha sido creado un nuevo administrador")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
