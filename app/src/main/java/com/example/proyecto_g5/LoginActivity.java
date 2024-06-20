807b7bfa8a04b12ea2f2d288c4fff77a52a8a430
Daniel <a20180805@pucp.edu.pe>
2024-06-20 17:47:26 -0500

    funciona log de login si no eres superadm

--- app/src/main/java/com/example/proyecto_g5/LoginActivity.java
+++ app/src/main/java/com/example/proyecto_g5/LoginActivity.java
@@ -181,7 +181,7 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         // Create log entry
-        crearLog(role, superadminId);
+        crearLog(role, nombre, superadminId);
 
         intent.putExtra("correo", email);
         startActivity(intent);
@@ -189,10 +189,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-
-    private void crearLog(String role, String superadminId) {
-        String descripcion = "El " + role + " se ha logueado";
-        String usuario = " (" + role + ")";
+    private void crearLog(String role, String nombre, String superadminId) {
+        String descripcion = "El " + role + " " + nombre + " se ha logueado";
+        String usuario = nombre + " (" + role + ")";
         Timestamp timestamp = Timestamp.now(); // Obtener el timestamp actual
 
         Llog log = new Llog(UUID.randomUUID().toString(), descripcion, usuario, timestamp);
@@ -217,8 +216,6 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
-
-
     private void mostrarOcultarContrasena(boolean mostrar) {
         if (mostrar) {
             // Mostrar contraseña
