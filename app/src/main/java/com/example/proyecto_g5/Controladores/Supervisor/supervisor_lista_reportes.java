4a5724e1ec331c0f8f130c8ead88f62c3d04c4ea
william <william24jes@hotmail.com>
2024-07-14 01:17:35 -0500

    ya guarda y no hay error al retroceder

--- app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_reportes.java
+++ app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_reportes.java
@@ -5,14 +5,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.proyecto_g5.R;
 import com.example.proyecto_g5.databinding.SupervisorListaReportesBinding;
 import com.example.proyecto_g5.dto.Reporte;
@@ -20,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,47 +32,58 @@ public class supervisor_lista_reportes extends Fragment implements MyAdapterList
     private List<Reporte> datalist;
     private MyAdapterListaReportes adapter;
     private FirebaseFirestore db;
-    private String guardarCodigositio;
-    private String guardarNumeroSerieEquipo;
+    private String codigoDeSitio;
+    private String numeroSerieEquipo;
 
     public supervisor_lista_reportes() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         supervisorListaReportesBinding = SupervisorListaReportesBinding.inflate(inflater, container, false);
+        db = FirebaseFirestore.getInstance();
 
         Bundle bundle = getArguments();
         if (bundle != null) {
-            String codigoSitio = bundle.getString("ACScodigo");
-            setGuardarCodigositio(codigoSitio);
-            String numeroSerieEquipo = bundle.getString("numero_serie_equipo");
-            setGuardarNumeroSerieEquipo(numeroSerieEquipo);
-
-            Log.d("supervisor_lista_reportes", "Código de sitio: " + codigoSitio);
+            codigoDeSitio = bundle.getString("ACScodigo");
+            numeroSerieEquipo = bundle.getString("numero_serie_equipo");
+            Log.d("supervisor_lista_reportes", "Código de sitio: " + codigoDeSitio);
             Log.d("supervisor_lista_reportes", "Número de serie equipo: " + numeroSerieEquipo);
-
-            if (codigoSitio != null && numeroSerieEquipo != null) {
-                GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
-                recyclerView = supervisorListaReportesBinding.recyclerViewReportesSupervisor;
-                recyclerView.setLayoutManager(gridLayoutManager);
-                datalist = new ArrayList<>();
-                adapter = new MyAdapterListaReportes(getActivity(), datalist, this);
-                recyclerView.setAdapter(adapter);
-                getDataFromFirestore(codigoSitio, numeroSerieEquipo);
-
-                NavController navController = NavHostFragment.findNavController(supervisor_lista_reportes.this);
-                supervisorListaReportesBinding.agregarReporte.setOnClickListener(view -> {
-                    navController.navigate(R.id.action_supervisor_lista_reportes_to_supervisor_nuevo_reporte);
-                });
-            } else {
-                Log.e("supervisor_lista_reportes", "Los argumentos recibidos son nulos");
-            }
-        } else {
-            Log.e("supervisor_lista_reportes", "No se recibieron argumentos");
         }
+
+        datalist = new ArrayList<>();
+        adapter = new MyAdapterListaReportes(getActivity(), datalist, this);
+        recyclerView = supervisorListaReportesBinding.recyclerViewReportesSupervisor;
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+        recyclerView.setAdapter(adapter);
+
+        if (codigoDeSitio != null && numeroSerieEquipo != null) {
+            getDataFromFirestore(codigoDeSitio, numeroSerieEquipo);
+        } else {
+            Log.e("supervisor_lista_reportes", "Código de sitio o número de serie son nulos");
+        }
+
+        NavController navController = NavHostFragment.findNavController(this);
+        supervisorListaReportesBinding.agregarReporte.setOnClickListener(view -> {
+            Bundle newBundle = new Bundle();
+            newBundle.putString("ACScodigo", codigoDeSitio);
+            newBundle.putString("numero_serie_equipo", numeroSerieEquipo);
+            navController.navigate(R.id.action_supervisor_lista_reportes_to_supervisor_nuevo_reporte, newBundle);
+        });
+
+        supervisorListaReportesBinding.BuscarReporte.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchList(newText);
+                return false;
+            }
+        });
 
         return supervisorListaReportesBinding.getRoot();
     }
@@ -79,50 +92,64 @@ public class supervisor_lista_reportes extends Fragment implements MyAdapterList
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String userId = user.getUid();
-            db = FirebaseFirestore.getInstance();
 
+            // Obtener el sitio específico
             db.collection("usuarios_por_auth")
                     .document(userId)
                     .collection("sitios")
-                    .document(codigoSitio)
-                    .collection("equipos")
-                    .document(numeroSerieEquipo)
-                    .collection("reportes")
-                    .addSnapshotListener((value, error) -> {
-                        if (error != null) {
-                            Toast.makeText(getContext(), "Error al obtener reportes", Toast.LENGTH_SHORT).show();
-                            Log.e("supervisor_lista_reportes", "Error al obtener reportes", error);
+                    .whereEqualTo("codigo", codigoSitio)
+                    .addSnapshotListener((sitiosTask, sitiosError) -> {
+                        if (sitiosError != null) {
+                            Toast.makeText(getContext(), "Error al obtener sitio", Toast.LENGTH_SHORT).show();
+                            Log.e("supervisor_lista_reportes", "Error al obtener sitio", sitiosError);
                             return;
                         }
 
-                        datalist.clear();
-                        for (DocumentSnapshot document : value.getDocuments()) {
-                            Reporte reporte = document.toObject(Reporte.class);
-                            if (reporte != null) {
-                                datalist.add(reporte);
-                            }
+                        for (DocumentSnapshot sitioDoc : sitiosTask.getDocuments()) {
+                            String sitioId = sitioDoc.getId();
+
+                            // Obtener reportes del equipo específico
+                            db.collection("usuarios_por_auth")
+                                    .document(userId)
+                                    .collection("sitios")
+                                    .document(sitioId)
+                                    .collection("equipos")
+                                    .document(numeroSerieEquipo)
+                                    .collection("reportes")
+                                    .addSnapshotListener((reportesTask, reportesError) -> {
+                                        if (reportesError != null) {
+                                            Toast.makeText(getContext(), "Error al obtener reportes", Toast.LENGTH_SHORT).show();
+                                            Log.e("supervisor_lista_reportes", "Error al obtener reportes", reportesError);
+                                            return;
+                                        }
+
+                                        datalist.clear();
+                                        for (DocumentSnapshot reporteDoc : reportesTask.getDocuments()) {
+                                            Reporte reporte = reporteDoc.toObject(Reporte.class);
+                                            if (reporte != null) {
+                                                datalist.add(reporte);
+                                                Log.d("supervisor_lista_reportes", "Reporte: " + reporte.getTitulo() + " listado correctamente.");
+                                            }
+                                        }
+                                        adapter.notifyDataSetChanged();
+                                    });
                         }
-                        adapter.notifyDataSetChanged();
                     });
         }
     }
 
+
     @Override
     public void onItemClick(Reporte item) {
-        NavController navController = NavHostFragment.findNavController(supervisor_lista_reportes.this);
+        NavController navController = NavHostFragment.findNavController(this);
         Bundle bundle = new Bundle();
-
-        bundle.putString("numero_serie_equipo", getGuardarNumeroSerieEquipo());
-        bundle.putString("ACScodigo", getGuardarCodigositio());
+        bundle.putString("numero_serie_equipo", numeroSerieEquipo);
+        bundle.putString("ACScodigo", codigoDeSitio);
         bundle.putString("codigoReporte", item.getCodigo());
 
-        Log.d("supervisor_lista_reportes", "Enviando código de reporte: " + item.getCodigo() +
-                ", Código de sitio: " + getGuardarCodigositio() +
-                ", Número de serie equipo: " + getGuardarNumeroSerieEquipo());
-
+        Log.d("supervisor_lista_reportes", "Enviando código de reporte: " + item.getCodigo());
         navController.navigate(R.id.action_supervisor_lista_reportes_to_supervisor_nuevo_reporte, bundle);
     }
-
 
     private void searchList(String text) {
         List<Reporte> dataSearchList = new ArrayList<>();
@@ -132,21 +159,5 @@ public class supervisor_lista_reportes extends Fragment implements MyAdapterList
             }
         }
         adapter.setSearchList(dataSearchList);
-    }
-
-    public String getGuardarCodigositio() {
-        return guardarCodigositio;
-    }
-
-    public void setGuardarCodigositio(String guardarCodigositio) {
-        this.guardarCodigositio = guardarCodigositio;
-    }
-
-    public String getGuardarNumeroSerieEquipo() {
-        return guardarNumeroSerieEquipo;
-    }
-
-    public void setGuardarNumeroSerieEquipo(String guardarNumeroSerieEquipo) {
-        this.guardarNumeroSerieEquipo = guardarNumeroSerieEquipo;
     }
 }
--- app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_nuevo_reporte.java
+++ app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_nuevo_reporte.java
@@ -1,113 +1,127 @@
 package com.example.proyecto_g5.Controladores.Supervisor;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.proyecto_g5.R;
 import com.example.proyecto_g5.databinding.SupervisorNuevoReporteBinding;
+import com.example.proyecto_g5.dto.Reporte;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link supervisor_nuevo_reporte#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
+
 public class supervisor_nuevo_reporte extends Fragment {
+
     private SupervisorNuevoReporteBinding supervisorNuevoReporteBinding;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseFirestore db;
+    private String codigoSitio;
+    private String numeroSerieEquipo;
 
     public supervisor_nuevo_reporte() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment supervisor_nuevo_reporte.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static supervisor_nuevo_reporte newInstance(String param1, String param2) {
-        supervisor_nuevo_reporte fragment = new supervisor_nuevo_reporte();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            codigoSitio = getArguments().getString("ACScodigo");
+            numeroSerieEquipo = getArguments().getString("numero_serie_equipo");
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         supervisorNuevoReporteBinding = SupervisorNuevoReporteBinding.inflate(inflater, container, false);
+        db = FirebaseFirestore.getInstance();
+        NavController navController = NavHostFragment.findNavController(this);
 
-        // Obtener referencias a los componentes de la vista
-        ScrollView scrollView = supervisorNuevoReporteBinding.scrollView;
-        TextInputEditText textInputEditText = supervisorNuevoReporteBinding.campoDescripcionReporte;
+        supervisorNuevoReporteBinding.button4.setOnClickListener(view -> {
+            if (validarCampos()) {
+                String titulo = supervisorNuevoReporteBinding.campoTTulo.getText().toString();
+                String descripcion = supervisorNuevoReporteBinding.campoDescripcionReporte.getText().toString();
 
-        // Agregar TextWatcher para desplazar el ScrollView hacia abajo mientras se escribe texto
-        textInputEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // No se necesita implementación aquí
-            }
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String userId = user.getUid();
+                String codigoReporte = generarCodigoReporte();
+                String fechaRegistro = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+                String estado = "Sin resolver";
+                String supervisor = user.getDisplayName(); // Obtener el nombre del supervisor
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // No se necesita implementación aquí
-            }
+                Reporte nuevoReporte = new Reporte(titulo, fechaRegistro, null, descripcion, null, estado, supervisor, codigoReporte);
 
-            @Override
-            public void afterTextChanged(Editable s) {
-                scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
+                db.collection("usuarios_por_auth")
+                        .document(userId)
+                        .collection("sitios")
+                        .document(codigoSitio)
+                        .collection("equipos")
+                        .document(numeroSerieEquipo)
+                        .collection("reportes")
+                        .document(codigoReporte)
+                        .set(nuevoReporte)
+                        .addOnSuccessListener(aVoid -> {
+                            Log.d("TAG", "Reporte guardado con ID: " + codigoReporte);
+                            Toast.makeText(requireContext(), "Reporte guardado", Toast.LENGTH_SHORT).show();
+                            navController.popBackStack(R.id.supervisor_lista_reportes, true);
+                            Bundle bundle = new Bundle();
+                            bundle.putString("ACScodigo", codigoSitio);
+                            bundle.putString("numero_serie_equipo", numeroSerieEquipo);
+                            navController.navigate(R.id.supervisor_lista_reportes, bundle);
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.w("TAG", "Error al agregar reporte", e);
+                            Toast.makeText(requireContext(), "Error al guardar reporte", Toast.LENGTH_SHORT).show();
+                        });
             }
         });
 
-        // Verificar los datos recibidos en el Bundle
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            String numeroSerieEquipo = bundle.getString("numero_serie_equipo");
-            String codigoSitio = bundle.getString("ACScodigo");
-            String codigoReporte = bundle.getString("codigoReporte");
-
-            // Log para verificar los valores
-            Log.d("supervisor_nuevo_reporte", "Número de serie del equipo: " + numeroSerieEquipo);
-            Log.d("supervisor_nuevo_reporte", "Código de sitio: " + codigoSitio);
-            Log.d("supervisor_nuevo_reporte", "Código del reporte: " + codigoReporte);
-        } else {
-            Log.e("supervisor_nuevo_reporte", "No se recibieron argumentos en el Bundle");
-        }
-
-
-        // Inflate the layout for this fragment
         return supervisorNuevoReporteBinding.getRoot();
     }
 
+    private boolean validarCampos() {
+        String titulo = supervisorNuevoReporteBinding.campoTTulo.getText().toString();
+        String descripcion = supervisorNuevoReporteBinding.campoDescripcionReporte.getText().toString();
+
+        boolean valid = true;
+
+        if (titulo.isEmpty()) {
+            supervisorNuevoReporteBinding.campoTTulo.setError("Ingrese el título");
+            valid = false;
+        } else {
+            supervisorNuevoReporteBinding.campoTTulo.setError(null);
+        }
+
+        if (descripcion.isEmpty()) {
+            supervisorNuevoReporteBinding.campoDescripcionReporte.setError("Ingrese la descripción");
+            valid = false;
+        } else {
+            supervisorNuevoReporteBinding.campoDescripcionReporte.setError(null);
+        }
+
+        return valid;
+    }
+
+    private String generarCodigoReporte() {
+        String fecha = new SimpleDateFormat("ddMMyyyyHHmmss", Locale.getDefault()).format(new Date());
+        int numeroAleatorio = (int) (Math.random() * 900) + 100; // genera un número entre 100 y 999
+        return fecha + numeroAleatorio;
+    }
 }
