63772d4ecb6d2184fd6d0963660d2fc7dfad0161
william <william24jes@hotmail.com>
2024-07-15 13:24:10 -0500

    ya funciona lista equipos

--- app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_equipos.java
+++ app/src/main/java/com/example/proyecto_g5/Controladores/Supervisor/supervisor_lista_equipos.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 import com.example.proyecto_g5.R;
 import com.example.proyecto_g5.databinding.SupervisorListaEquiposBinding;
 import com.example.proyecto_g5.dto.Equipo;
+import com.example.proyecto_g5.dto.Sitio;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -85,6 +86,7 @@ public class supervisor_lista_equipos extends Fragment implements MyAdapterLista
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             correo = getArguments().getString("correo");
+            codigoDeSitio = getArguments().getString("ACScodigo");
         }
 
 
@@ -149,47 +151,34 @@ public class supervisor_lista_equipos extends Fragment implements MyAdapterLista
         if (user != null) {
             String userId = user.getUid();
             String codigoSitio = getArguments().getString("ACScodigo");
+            Log.d("message",codigoSitio);
             setCodigoDeSitio(codigoSitio);
 
-            db.collection("usuarios_por_auth")
-                    .document(userId)
-                    .collection("sitios")
-                    .whereEqualTo("codigo", codigoSitio)
-                    .addSnapshotListener((task, error) -> {
-                        if (error != null) {
-                            Log.d("msg-test", "Error al obtener sitio: ", error);
-                            Toast.makeText(getContext(), "Error al obtener sitio", Toast.LENGTH_SHORT).show();
+
+            db.collection("sitios")
+                    .document(codigoDeSitio)
+                    .collection("equipos")
+                    .addSnapshotListener((equiposTask, equiposError) -> {
+                        if (equiposError != null) {
+                            Log.d("msg-test", "Error al obtener equipos: ", equiposError);
+                            Toast.makeText(getContext(), "Error al obtener equipos", Toast.LENGTH_SHORT).show();
                             return;
                         }
-                        for (DocumentSnapshot document : task.getDocuments()) {
-                            String sitioId = document.getId();
 
-                            db.collection("usuarios_por_auth")
-                                    .document(userId)
-                                    .collection("sitios")
-                                    .document(sitioId)
-                                    .collection("equipos")
-                                    .addSnapshotListener((equiposTask, equiposError) -> {
-                                        if (equiposError != null) {
-                                            Log.d("msg-test", "Error al obtener equipos: ", equiposError);
-                                            Toast.makeText(getContext(), "Error al obtener equipos", Toast.LENGTH_SHORT).show();
-                                            return;
-                                        }
+                        datalist.clear();
 
-                                        datalist.clear();
-
-                                        for (DocumentSnapshot equipoDoc : equiposTask.getDocuments()) {
-                                            Equipo equipo = equipoDoc.toObject(Equipo.class);
-                                            datalist.add(equipo);
-                                            Log.d("msg-test", "Equipo: " + equipo.getNombre_tipo() + " listado correctamente.");
-                                        }
-
-                                        adapter.notifyDataSetChanged();
-                                    });
+                        for (DocumentSnapshot equipoDoc : equiposTask.getDocuments()) {
+                            Equipo equipo = equipoDoc.toObject(Equipo.class);
+                            datalist.add(equipo);
+                            Log.d("msg-test", "Equipo: " + equipo.getNombre_tipo() + " listado correctamente.");
                         }
+
+                        adapter.notifyDataSetChanged();
                     });
         }
+
     }
+
 
 
     @Override
